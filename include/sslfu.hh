ef3d41436bf14a0633bfa24c29536c5144c34662
Florian Wagner <fl.wagner@uni-tuebingen.de>
2020-05-15 16:33:40 +0200

    sslfu: Reworked.

--- include/sslfu.hh
+++ include/sslfu.hh
@@ -16,6 +16,7 @@
 #define SSLFU_HH
 
 #include <istream>
+#include <memory>
 #include <streambuf>
 #include <string>
 #include <stdexcept>
@@ -101,69 +102,98 @@ namespace sslfu {
 
   class openssl_error : public std::runtime_error {
     protected:
-      class queue_item {
-        public:
-          const unsigned long code;
-          const char* lib;
-          const char* func;
-          const char* reason;
-
-          queue_item(const unsigned long& c)
-            : code(c),
-              lib(ERR_lib_error_string(c)),
-              func(ERR_func_error_string(c)),
-              reason(ERR_reason_error_string(c)) {
-            /* empty */
-          };
-
-          const std::string what() const {
-            std::string ret;
-            if (func) {
-              ret += func;
-              ret += " ";
-            }
-            return ret + "in " + lib + ": " + reason;
-          };
-      };
-      const std::vector<queue_item> queue;
-
-      openssl_error
-          (const std::string& w, const std::vector<queue_item>& q)
-            : std::runtime_error(construct_what(w, q)), queue(q) {
-        /* empty */
-      };
-
-      static const std::string construct_what
-          (const std::string& w,
-           const std::vector<queue_item>& q) {
-        std::string what_arg = w;
-        for (auto item : q) {
-          what_arg += "\n    " + item.what();
+      std::string construct_what(const std::string& w, unsigned long c) const {
+        const char* lib = ERR_lib_error_string(c);
+        const char* func = ERR_func_error_string(c);
+        const char* reason = ERR_reason_error_string(c);
+        std::string ret = w + ": ";
+        if (func) {
+          ret += func;
+          ret += " ";
         }
-        return what_arg;
+        return ret + "in " + lib + ": " + reason;
+      };
+
+      unsigned long clear_error_queue() const {
+         unsigned long err = ERR_get_error();
+        if (!err)
+          throw std::runtime_error("Bla");
+        ERR_clear_error();
+         return err;
       };
 
     public:
-      openssl_error(openssl_error&& o)
-        : std::runtime_error(o.what()), queue(std::move(o.queue)) {
-        /* empty */
+      const unsigned long code;
+      openssl_error(const std::string& w, const unsigned long c)
+        : code(c), std::runtime_error(construct_what(w, c)) { /* empty */ };
+      openssl_error(const std::string& w)
+        : openssl_error(w, clear_error_queue()) { /* empty */ };
+  };
+
+  namespace detail {
+    template<typename T, int(&UPREF)(T*), void(&FREE)(T*)>
+    class refcntobj {
+      protected:
+        T* _ptr = 0;
+      public:
+        refcntobj() { /* empty */ };
+        refcntobj(const refcntobj& o) : _ptr(o._ptr) { UPREF(_ptr); };
+        refcntobj& operator=(const refcntobj& o) {
+          FREE(_ptr); _ptr = o._ptr; UPREF(_ptr); return *this;
+        };
+        ~refcntobj() { FREE(_ptr); };
+        T* ptr() const { return _ptr; };
+    };
+  };
+
+  class pkey : public detail::refcntobj<EVP_PKEY,EVP_PKEY_up_ref,EVP_PKEY_free> {
+    public:
+      using detail::refcntobj<EVP_PKEY,EVP_PKEY_up_ref,EVP_PKEY_free>::refcntobj;
+      pkey(FILE* fp) {
+        _ptr = PEM_read_PrivateKey(fp, 0, 0, 0);
+        if (!_ptr)
+          throw openssl_error("Error loading private key");
+      };
+  };
+
+  class x509 : public detail::refcntobj<X509,X509_up_ref,X509_free> {
+    public:
+      typedef std::vector<x509> chain;
+
+      using detail::refcntobj<X509,X509_up_ref,X509_free>::refcntobj;
+      x509(FILE* fp) {
+        _ptr = PEM_read_X509(fp, 0, 0, 0);
+        if (!_ptr)
+          throw openssl_error("Error loading certificate");
       };
 
-      static openssl_error with_error_queue(const std::string& what) {
-        std::vector<queue_item> q;
-        unsigned long err = 0;
-        while((err = ERR_get_error()) != 0) {
-          q.push_back(queue_item(err));
+      static chain load_chain(FILE* fp) {
+        chain c;
+        while (1) {
+          try {
+            c.emplace_back(fp);
+          }
+          catch (openssl_error& exc) {
+            if (exc.code != 167768172)
+              throw exc;
+            break;
+          }
         }
-        return openssl_error(what, q);
+        return c;
       };
+  };
+
+  void init() {
+    SSL_library_init();
+    SSL_load_error_strings();
+    OPENSSL_config(0);
   };
 
   class clientstream : public std::iostream {
     protected:
       SSL_CTX* _ctx = 0;
       BIO* _conn = 0;
-      SSL *_ssl = 0;
+      SSL* _ssl = 0;
       detail::biobuf _buf;
 
       std::string _ciphers =
@@ -172,18 +202,14 @@ namespace sslfu {
       long _options = SSL_OP_ALL | SSL_OP_NO_COMPRESSION;
       int _verify_mode = SSL_VERIFY_PEER;
       int _verify_depth = 8;
-      std::string _CAfile;
-      std::string _CApath;
-      std::string _cert;
+      bool _load_default_verify = true;
+      x509::chain _cachain;
+      std::shared_ptr<x509> _clcert = 0;
+      std::shared_ptr<pkey> _clpkey = 0;
 
     public:
-      clientstream()
-          : std::iostream(&_buf),
-            _CAfile(X509_get_default_cert_file()),
-            _CApath(X509_get_default_cert_dir()) {
-        SSL_library_init();
-        SSL_load_error_strings();
-        OPENSSL_config(0);
+      clientstream() : std::iostream(&_buf) {
+        /* empty */
       };
 
       clientstream(const std::string& hostname, const std::string& port)
@@ -195,56 +221,72 @@ namespace sslfu {
         SSL_CTX_free(_ctx);
       };
 
-      void CAfile (const std::string&& val) { _CAfile = val; };
-      void CApath (const std::string&& val) { _CApath = val; };
-      void cert   (const std::string&& val) { _cert   = val; };
+      void disable_default_verify() {
+        _load_default_verify = false;
+      };
+      void cacert (const x509& c) {
+        _cachain.resize(std::max(_cachain.size(), x509::chain::size_type(1)));
+        _cachain[0] = c;
+      };
+      void cacert (const x509::chain& c) {
+        _cachain = c;
+      };
+      void clcert (const x509& c, const pkey& p) {
+        _clcert = std::make_shared<x509>(c);
+        _clpkey = std::make_shared<pkey>(p);
+      };
 
       void connect(const std::string& hostname, const std::string& port) {
         ERR_clear_error();
 
         const SSL_METHOD* method = TLS_client_method();
         if (method == 0)
-          throw openssl_error::with_error_queue(
-            "Error constructing OpenSSL SSL_METHOD");
+          throw openssl_error("Error constructing OpenSSL SSL_METHOD");
 
         _ctx = SSL_CTX_new(method);
         if (_ctx == 0)
-          throw openssl_error::with_error_queue(
-              "Error constructing OpenSSL SSL_CTX");
+          throw openssl_error("Error constructing OpenSSL SSL_CTX");
 
         if (SSL_CTX_set_min_proto_version(_ctx, TLS1_2_VERSION) != 1)
-          throw openssl_error::with_error_queue(
+          throw openssl_error(
               "Error setting TLSv1.2 as minimum protocol version");
 
         SSL_CTX_set_options(_ctx, _options);
         SSL_CTX_set_verify(_ctx, _verify_mode, 0);
         SSL_CTX_set_verify_depth(_ctx, _verify_depth);
 
-        if (SSL_CTX_load_verify_locations(
-              _ctx,
-              _CAfile.empty() ? 0 : _CAfile.c_str(),
-              _CApath.empty() ? 0 : _CApath.c_str()) != 1)
-          throw openssl_error::with_error_queue(
-            "Error loading CAfile and/or CApath");
+        if (_load_default_verify &&
+            SSL_CTX_set_default_verify_paths(_ctx) != 1)
+          throw openssl_error(
+            "Error loading default CA file and path");
 
-        if (!_cert.empty()) {
-          if (SSL_CTX_use_certificate_file(
-                _ctx, _cert.c_str(), SSL_FILETYPE_PEM) != 1)
-            throw openssl_error::with_error_queue(
+        X509_STORE* store = SSL_CTX_get_cert_store(_ctx);
+        for (auto cert : _cachain)
+          if (X509_STORE_add_cert(store, cert.ptr()) != 1)
+            throw openssl_error("Error installing verification chain");
+
+        if (_clcert && _clpkey) {
+          if (SSL_CTX_use_certificate(_ctx, _clcert->ptr()) != 1)
+            throw openssl_error(
               "Error loading client certificate");
+          if (SSL_CTX_use_PrivateKey(_ctx, _clpkey->ptr()) != 1)
+            throw openssl_error(
+              "Error loading client certificate private key");
+          if (SSL_CTX_check_private_key(_ctx) != 1)
+            throw openssl_error(
+              "Client certificate and/or private key invalid");
         }
 
         _conn = BIO_new_ssl_connect(_ctx);
         if (_conn == 0)
-          throw openssl_error::with_error_queue(
-            "Error constructing SSL connection");
+          throw openssl_error("Error constructing SSL connection");
 
         BIO_set_conn_hostname(_conn, hostname.c_str());
         BIO_set_conn_port(_conn, port.c_str());
 
         BIO_get_ssl(_conn, &_ssl);
         if (_ssl == 0)
-          throw openssl_error::with_error_queue(
+          throw openssl_error(
             "Error retrieving SSL pointer from connection");
 
         X509_VERIFY_PARAM* param = SSL_get0_param(_ssl);
@@ -256,24 +298,20 @@ namespace sslfu {
 
         if (X509_VERIFY_PARAM_set1_host(
               param, hostname.c_str(), hostname.length()) != 1)
-          throw openssl_error::with_error_queue(
+          throw openssl_error(
             "Error setting hostname for certificate verification");
 
         if (SSL_set_cipher_list(_ssl, _ciphers.c_str()) != 1)
-          throw openssl_error::with_error_queue(
-            "Error setting SSL ciphers");
+          throw openssl_error("Error setting SSL ciphers");
 
         if (SSL_set_tlsext_host_name(_ssl, hostname.c_str()) != 1)
-          throw openssl_error::with_error_queue(
-            "Error setting SNI ClientHello value");
+          throw openssl_error("Error setting SNI ClientHello value");
 
         if (BIO_do_connect(_conn) != 1)
-          throw openssl_error::with_error_queue(
-            "Error opening connection");
+          throw openssl_error("Error opening connection");
 
         if (BIO_do_handshake(_conn) != 1)
-          throw openssl_error::with_error_queue(
-            "Error on SSL handshake");
+          throw openssl_error("Error on SSL handshake");
 
         _buf.bio(_conn);
       };
